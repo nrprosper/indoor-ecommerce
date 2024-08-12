e15c09514e57e90da9e7f388d7f4a6bb45c80683
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-12 21:31:55 +0200

    fixed padding on category card

--- src/Components/CategoryCard.tsx
+++ src/Components/CategoryCard.tsx
@@ -14,7 +14,7 @@ const CategoryCard = ({title, className, image}:CategoryCardProps) => {
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat"
             }}
-            className={`${className} px-12 py-12 flex`}>
+            className={`${className} px-8 py-8 md:px-12 md:py-12 flex`}>
             <div>
                 <h3 className="text-theme-neutral-7 font-medium text-[1.75rem] md:text-4xl mb-0 md:mb-3">{title}</h3>
                 <Linkcta className="w-[106px] md:text-[1rem]" to={"/shop"}>Shop now</Linkcta>
