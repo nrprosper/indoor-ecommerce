bd0282d0ea2b4de799ed33c46fe72c6b9dea8f4e
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-12 21:26:14 +0200

    fixed categories and container

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -2,7 +2,7 @@ import {PropsWithChildren} from "react";
 
 const Container = ({children}:PropsWithChildren) => {
     return(
-        <div className="container mx-auto px-8 xl:px-9">
+        <div className="container mx-auto px-4 lg:px-8 xl:px-9">
             {children}
         </div>
     )
