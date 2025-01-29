b404511b23b75eedf9d0078f500d4c95c5a45f2b
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-29 21:29:31 +0200

    chore: migrate from react to nextjs

--- src/Constants/icons.ts
+++
@@ -1,8 +0,0 @@
-import storeWide from "../assets/icons/storeWide.svg";
-import search from "../assets/icons/search.svg";
-import profile from "../assets/icons/profile.svg";
-import bag from "../assets/icons/bag.svg";
-
-
-
-export {storeWide, search, profile, bag}
