b3f92c23e10be1a164f668e3326b8e4df2d2bd4e
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-01-30 16:00:14 +0200

    chore(tailwind): extended default tailwind to include my colors and shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
   content: [
@@ -8,11 +9,66 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)', ...fontFamily.sans],
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
+        spaceGrotesk: ['var(--font-space-grotesk)', ...fontFamily.sans],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'th-primary': '#000000',
+        'th-blue': '#377DFF',
+        'th-green': '#38CB89',
+        'th-yellow': '#FFAB00',
+        'th-red': '#FF5630',
+        'th-neutral': {
+          100: '#FEFEFE',
+          200: '#F3F5F7',
+          300: '#E8ECEF',
+          400: '#6C7275',
+          500: '#343839',
+          600: '#232627',
+          700: '#141718'
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
