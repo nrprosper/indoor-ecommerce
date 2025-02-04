296cd6ecb6aa18de42b79628136a6ef19ed96790
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-02-05 01:29:49 +0200

    refactor: can dynamically add the change the action label

--- src/components/action.tsx
+++ src/components/action.tsx
@@ -9,12 +9,12 @@ interface ActionProps {
     className?: string
 }
 
-const Action = ({ to, label, variant='black', className }: ActionProps) => {
+const Action = ({ to, label='shop now', variant='black', className }: ActionProps) => {
     return(
         <Link href={""} className={cn(
             `duration-200 transition-all font-medium flex items-center gap-2 text-sm py-[0.5px] border-b-2 hover:gap-3 hover:text-base ${variant === 'blue' ? 'text-th-blue border-b-th-blue' : 'text-th-primary border-b-th-primary'}`, className
         )}>
-            <span className="block">Shop now</span>
+            <span className="block capitalize">{label}</span>
             <MoveRight />
         </Link>
     )
