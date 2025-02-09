1cff478300d0f82c61fb9c8d3859376290cc6445
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-02-09 17:18:28 +0200

    refactor: change the project structure to handle multiple layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,26 +22,7 @@ export default function RootLayout({
       <body
           className={`${inter.variable} ${poppins.variable} ${spaceGrotesk.variable} antialiased`}
       >
-      <header>
-          <div className="px-8 md:px-0 bg-th-neutral-200 h-10 flex justify-center sm:justify-between items-center md:relative md:justify-center">
-              <div className="flex items-center gap-3">
-                  <div className="flex items-center gap-2">
-                      <TicketPercent />
-                      <h3 className="font-semibold">30% off storewide — Limited time! </h3>
-                  </div>
-                  <Action variant="blue" />
-              </div>
-              <button className="hidden sm:inline-flex md:absolute md:right-8">
-                  <Close size={20} />
-              </button>
-          </div>
-          <Navigation />
-      </header>
-        <main>
-            {children}
-        </main>
-
-      <Footer />
+      {children}
       </body>
     </html>
   );
